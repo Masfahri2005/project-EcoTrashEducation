402b95a29f81ec1bf14f784a09bbcd1251c145e4
Masfahri2005 <fakhrizalgarnindyo>
2024-12-03 22:48:19 +0700

    first commit

--- js/js.js
+++ js/js.js
@@ -1,12 +1,4 @@
-// dropdown menu
-const toggleBtn = document.querySelector(".toggle_btn");
-const toggleIcon = document.querySelector(".toggle_btn i");
-const dropdownMenu = document.querySelector(".dropdown_menu");
-
-toggleBtn.onclick = () => {
-  dropdownMenu.classList.toggle("open");
-  const isOpen = dropdownMenu.classList.contains("open");
-  toggleIcon.classList = isOpen
-    ? "fa-sharp fa-solid fa-xmark"
-    : "fa-sharp fa-solid fa-bars";
-};
+document.getElementById('menu-toggle').addEventListener('click', function() {
+  this.classList.toggle('active');
+  document.querySelector('.links').classList.toggle('active');
+});
